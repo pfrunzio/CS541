df3fa12a6a5e34cf0d9f86a9cb6364ece3afc7c3
Paola Frunzio <pfrunzio@wpi.edu>
2025-02-08 22:20:33 -0500

    more hw1

--- HW1/homework1.py
+++ HW1/homework1.py
@@ -1,19 +1,21 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 def problem_1a (A, B):
     return A + B
 
 def problem_1b (A, B, C):
-    return ...
+    return A * B + C.T
 
 def problem_1c (x, y):
-    return ...
+    return x.T @ y # inner product
 
 def problem_1d (A, j):
-    return ...
+    return np.sum(A[::2, j])
 
 def problem_1e (A, c, d):
-    return ...
+    return np.mean(A[np.nonzero((A >= c) & (A <= d))])
 
 def problem_1f (x, k, m, s):
     return ...
@@ -44,3 +46,47 @@ def train_age_regressor ():
 
     # Report fMSE cost on the training and testing data (separately)
     # ...
+
+
+def question4a():
+    #load data
+    data = np.load('PoissonX.npy')
+
+    #plot empirical distribution
+    plt.figure(figsize=(8, 6))
+    plt.hist(data, bins=20, density=True, alpha=0.6, color='g', label='Empirical Distribution')
+
+    plt.xlabel('Value')
+    plt.ylabel('Density')
+    plt.title('Empirical Probability Distribution')
+    # plt.legend()
+    # plt.show()
+
+    #set alt rate parameters
+    rate_params = [2.5, 3.1, 3.7, 4.3]
+    x_values = np.arange(0, int(np.max(data)) + 1)
+
+    #plot poisson distributions
+    for l in rate_params:
+        poisson_pmf = stats.poisson.pmf(x_values, l)
+        plt.plot(x_values, poisson_pmf, label=f'Poisson λ={l}', lw=2)
+
+    plt.xlabel('Value')
+    plt.ylabel('Density')
+    # plt.title('Poisson Distributions')
+
+    plt.title('Comparison of Empirical and Poisson Distributions')
+    plt.legend()
+    plt.show()
+
+def question4b():
+    mu = 1
+    sigma = 1.268857754
+
+    #calculate cumulative distribution function at 0
+    probY = stats.norm.cdf(0, loc=mu, scale=sigma)
+
+    #P(Y > 0)
+    probY_gt_0 = 1 - probY
+
+    print(probY_gt_0)
--- HW1/test.py
+++ HW1/test.py
@@ -3,7 +3,19 @@
 import numpy as np
 from homework1 import *
 
-A = np.array([1, 1, 1])
-B = np.array([0, 1, 1])
+A = np.array([[1, 1],
+             [1, 1]])
+B = np.array([[0, 1],
+             [1, 1]])
+C = np.array([[0, 2],
+             [2, 2]])
+D = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
 
-print(problem_1a(A, B))
+# print(problem_1a(A, B))
+# print(problem_1b(A, B, C))
+# print(problem_1d(C, 1))
+# print(problem_1e(D, 3, 7))
+
+question4b()
